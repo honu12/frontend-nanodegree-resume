5b38053760649ac09fba5fe77eb47df4543fa304
Honu <honu.menehune@gmail.com>
2015-08-12 16:58:21 -0700

    Added bio information, not displaying correctly yet

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = "Honu";
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+
+var bio = {
+	"name" : "Honu",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "555-555-5555",
+		"e-mail" : "honu.menehune@gmail.com",
+		"github" : "https://github.com/honu12",
+		"twitter" : "",
+		"location" : "Hawaii"
+	},
+	"welcomeMessage" : "lorem",
+	"skills" : ["awesomeness", "programming", "teaching", "JS"],
+	"bioPic" : "images/turtle.jpg"
+};
 
 $("#main").append("Honu");
+$("#topContacts").append(bio);
 
-var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-var role = "Web Developer";
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var HTMLcontactGeneric = HTMLcontactGeneric.replace("%data%", bio.mobile);
+var HTMLmobile = HTMLmobile.replace("%data%", bio.mobile);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
