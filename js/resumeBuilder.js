a354c9eb742c06a7f43d0aca640d8a412b3f324c
Honu <honu.menehune@gmail.com>
2015-08-13 14:06:19 -0700

    added #topContacts section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,31 +1,32 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var skills = ["awesomeness", "programming", "teaching", "JS"];
-
 var bio = {
 	"name" : "Honu",
 	"role" : "Web Developer",
 	"contacts" : {
-		"mobile" : "555-555-5555",
-		"e-mail" : "honu.menehune@gmail.com",
+		"mobile" : "(808) 532-8700",
+		"email" : "honu.menehune@gmail.com",
 		"github" : "https://github.com/honu12",
-		"twitter" : "",
+		"twitter" : "@Honu12mene",
 		"location" : "Hawaii"
 	},
 	"welcomeMessage" : "lorem",
 	"skills" : ["awesomeness", "programming", "teaching", "JS"],
 	"bioPic" : "images/turtle.jpg"
-};
+}
 
-$("#main").append("Honu");
-$("#topContacts").append(bio);
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-var HTMLcontactGeneric = HTMLcontactGeneric.replace("%data%", bio.mobile);
-var HTMLmobile = HTMLmobile.replace("%data%", bio.mobile);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+$("#topContacts").append(formattedMobile + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
