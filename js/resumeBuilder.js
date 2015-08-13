b23097413d27cd4eedc6297d2df1493efb815ead
Honu <honu.menehune@gmail.com>
2015-08-13 14:59:19 -0700

    Completed the Bio Section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// Bio Object
+
 var bio = {
 	"name" : "Honu",
 	"role" : "Web Developer",
@@ -11,11 +13,12 @@ var bio = {
 		"twitter" : "@Honu12mene",
 		"location" : "Hawaii"
 	},
-	"welcomeMessage" : "lorem",
-	"skills" : ["awesomeness", "programming", "teaching", "JS"],
+	"welcomeMessage" : "There are some Hawaiian legends which said that turtles were the first to guide the Polynesians to the Hawaiian islands, while other myths attribute that to the golden plover.",
+	"skills" : ["HTML", "CSS", "Javascript", "JQuery", "PHP", "Python"],
 	"bioPic" : "images/turtle.jpg"
 }
 
+// Handler for the Bio Object
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -26,7 +29,21 @@ var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
 var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
 var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+// Display Bio 
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
 $("#topContacts").append(formattedMobile + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
+
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+$("#header").append(HTMLskillsStart);
+for (var e in bio.skills) {
+      var formattedSkill = HTMLskills.replace("%data%", bio.skills[e]);
+      $("#skills").append(formattedSkill);
+    }
